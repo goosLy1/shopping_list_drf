a9ca3ce90e48d7bca658c3c2918778e1302f9440
goosLy1 <gray.goosly@gmail.com>
2023-03-11 00:37:41 +0300

    Practice with APIView without serializer

--- shopping_list_drf/shopping/views.py
+++ shopping_list_drf/shopping/views.py
@@ -1,8 +1,26 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Shopping
 from .serializers import ShoppingSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-class ShoppingApiView(generics.ListAPIView):
-    queryset = Shopping.objects.all()
-    serializer_class = ShoppingSerializer
+
+class ShoppingApiView(APIView):
+    def get(self, request):
+        lst = Shopping.objects.all().values()
+
+        return Response({'shopping_lists': list(lst)})
+    
+    def post(self, request):
+        new_list = Shopping.objects.create(
+            label = request.data['label'],
+            user_id = request.data['user_id']
+        )
+        return Response({'shopping_list': model_to_dict(new_list)})
+
+
+# class ShoppingApiView(generics.ListAPIView):
+#     queryset = Shopping.objects.all()
+#     serializer_class = ShoppingSerializer
